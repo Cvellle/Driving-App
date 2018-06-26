25b9d06a1b481a5dcf58dc9b9e8f7a66090334b5
Cvellle <cuekisa@yahoo.com>
2018-06-26 16:38:52 +0200

    A
    A

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const routes = JSON.parse(localStorage.getItem('routes')) || [];
 
 
 
+// MAIN COMPONENT
 
 class App extends React.Component {
  
@@ -20,7 +21,8 @@ class App extends React.Component {
      this.done = this.done.bind(this); 
 }
  
- 
+  // ADD NEW ROUTES
+
    add(e) {
      const routes = this.props.routes;
      
@@ -36,7 +38,8 @@ class App extends React.Component {
 
    }
    
- 
+  // DELETE ROUTES
+
    done(route) {
      routes.splice(routes.indexOf(route), 1);
      localStorage.setItem('routes', JSON.stringify(routes));
@@ -78,7 +81,7 @@ class App extends React.Component {
             
           </div>
          
-          <ContactBody /> 
+          <Details /> 
          
        </div>
      );
@@ -89,6 +92,10 @@ class App extends React.Component {
 
 
 
+
+
+  // ROUTE IN LIST
+
   class RouteItem extends React.Component{
 
     constructor(props) {
@@ -97,16 +104,14 @@ class App extends React.Component {
        this.show = this.show.bind(this);
      }
 
-    componentDidMount() {
-   
-    }
-    
-    
+
     done() {
         this.props.done(this.props.route);
      }
     
-    
+
+    // DISPLAY MAP
+
     show(e) {
       add.style.display="none";  
       map.style.display="block"; 
@@ -118,71 +123,73 @@ class App extends React.Component {
       const start = e.target.previousElementSibling.previousElementSibling.innerHTML;
       const end = e.target.previousElementSibling.innerHTML;
       
+      // GET DIRECTIONS
+
       setTimeout(function(e){
       
-      
-      let directionsDisplay;
-      const directionsService = new google.maps.DirectionsService();
-       
-      var map;
-
-      const belgrade = new google.maps.LatLng(44.8089237, 20.4813078);
-      const mapOptions = {
-        zoom: 6,
-        center: belgrade
-      }
-      
-      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-   
-      directionsDisplay = new google.maps.DirectionsRenderer();
-
-      const waypts = [];
-
-      const request = {
-          origin: start,
-          destination: end,
-          waypoints: waypts,
-          optimizeWaypoints: true,
-          travelMode: google.maps.TravelMode.DRIVING
-      };
-  
-      directionsService.route(request, function(response, status) {
-    
-        if (status == google.maps.DirectionsStatus.OK) {
-          
-             directionsDisplay.setDirections(response);
-          
-
-          const length = Math.round(directionsDisplay.getDirections().routes[directionsDisplay.getRouteIndex()].legs[0].distance.value / 1000);   
-          const time = (length/80).toFixed(2);
-          const timearray = time.split('.').map(parseFloat);
-
-          const hours = time.toString()[0];
-          const hours2 = `${Number(time.toString()[0])}${Number(time.toString()[1])}`;
-
-          const mins = `${time.toString()[2]}${time.toString()[3]}`;
-          const mins2 = `${time.toString()[3]}${time.toString()[4]}`;
-
-          const minsfin = (Number(mins) * 60 / 100).toFixed(0);
-          const minsfin2 = (Number(mins2) * 60 / 100).toFixed(0);
-
-          if (hours<=1) {
-            output.innerHTML = length + " km. About " + hours + " hour and " + mins + " mins";
-          } 
-
-          else if (time.toString().length == 5) {
-            output.innerHTML = length + " km. About " + hours2 + " hour and " + minsfin2 + " mins";
-          } 
-
-          else output.innerHTML = length + " km. About " + hours + " hours and " + minsfin + " mins";
-
-          const route = response.routes[0];
-        }
-    });
-  
-      directionsDisplay.setMap(map);
+        let directionsDisplay;
+        const directionsService = new google.maps.DirectionsService();
         
-      setTimeout(function(e){output.style.display="block";},100);
+        var map;
+
+        const belgrade = new google.maps.LatLng(44.8089237, 20.4813078);
+        const mapOptions = {
+          zoom: 6,
+          center: belgrade
+        }
+        
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+        directionsDisplay = new google.maps.DirectionsRenderer();
+
+        const waypts = [];
+
+        const request = {
+            origin: start,
+            destination: end,
+            waypoints: waypts,
+            optimizeWaypoints: true,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        
+        directionsService.route(request, function(response, status) {
+      
+          if (status == google.maps.DirectionsStatus.OK) {
+            
+            directionsDisplay.setDirections(response);
+            
+            // COUNT DISTANCE AND TIME
+
+            const length = Math.round(directionsDisplay.getDirections().routes[directionsDisplay.getRouteIndex()].legs[0].distance.value / 1000);   
+            const time = (length/80).toFixed(2);
+            const timearray = time.split('.').map(parseFloat);
+
+            const hours = time.toString()[0];
+            const hours2 = `${Number(time.toString()[0])}${Number(time.toString()[1])}`;
+
+            const mins = `${time.toString()[2]}${time.toString()[3]}`;
+            const mins2 = `${time.toString()[3]}${time.toString()[4]}`;
+
+            const minsfin = (Number(mins) * 60 / 100).toFixed(0);
+            const minsfin2 = (Number(mins2) * 60 / 100).toFixed(0);
+
+            if (hours<=1) {
+              output.innerHTML = length + " km. About " + hours + " hour and " + mins + " mins";
+            } 
+
+            else if (time.toString().length == 5) {
+              output.innerHTML = length + " km. About " + hours2 + " hour and " + minsfin2 + " mins";
+            } 
+
+            else output.innerHTML = length + " km. About " + hours + " hours and " + minsfin + " mins";
+
+            const route = response.routes[0];
+          }
+      });
+    
+        directionsDisplay.setMap(map);
+          
+        setTimeout(function(e){output.style.display="block";},100);
         
   }, 0) 
 
@@ -203,10 +210,15 @@ class App extends React.Component {
        }
    }
    
+
+
+
+
    
   
+   // MAP COMPONENT
 
-   class ContactBody extends React.Component {
+   class Details extends React.Component {
   
     constructor(props) {
        super(props);
@@ -235,7 +247,9 @@ class App extends React.Component {
    componentWillMount() {
       this.getGoogleMaps();
     }
-     
+    
+   //HIDE MAP - GO BACK
+
    hide(e) {
      map.style.display="none";
      e.target.style.display ="none";
